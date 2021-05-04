e6967bd4308083af7c45faa1cc1057dbceee5a3c
Maija <maija.utriainen@gmail.com>
2021-05-04 10:20:22 +0200

    trying to serialize query results but doesn't work

--- browser app/hormoneQuery.js
+++ browser app/hormoneQuery.js
@@ -1,4 +1,6 @@
 
+const myEngine = Comunica.newEngine();
+
 // from wikidata
 query1 = `
 PREFIX wdp: <http://www.wikidata.org/prop/direct/>
@@ -21,10 +23,63 @@ sources = [];
 sources.push('https://query.wikidata.org/sparql');
 //sources.push('https://api.nextprot.org/sparql');
 
-Comunica.newEngine().query(query1, {sources: sources,})
+var dataJSON = {};
+
+myEngine.query(query1, {sources: sources,})
 	.then(function (result) {
     result.bindingsStream.on('data', function (data) {
         // Each variable binding is an RDFJS term
         console.log(data.get('?item').value);
     });
+			
+	dataJSON = myEngine.resultToString(result, 'application/json');
 });
+
+
+console.log(dataJSON);
+
+// this is for cytoscape.js
+
+/* var cy = cytoscape({ // variable cy is the graph?
+
+  container: document.getElementById('cy'), // container to render in
+
+  elements: [ // list of graph elements to start with
+    { // node a
+      data: { id: 'a' }
+    },
+    { // node b
+      data: { id: 'b' }
+    },
+    { // edge ab
+      data: { id: 'ab', source: 'a', target: 'b' }
+    }
+  ],
+
+  style: [ // the stylesheet for the graph
+    {
+      selector: 'node',
+      style: {
+        'background-color': '#666',
+        'label': 'data(id)'
+      }
+    },
+
+    {
+      selector: 'edge',
+      style: {
+        'width': 3,
+        'line-color': '#ccc',
+        'target-arrow-color': '#ccc',
+        'target-arrow-shape': 'triangle',
+        'curve-style': 'bezier'
+      }
+    }
+  ],
+
+  layout: {
+    name: 'grid',
+    rows: 1
+  }
+
+}); */
